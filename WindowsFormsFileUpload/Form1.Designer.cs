7d4ef43f4f22125d5f15f25f63cfb9465a881a54
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 12:59:13 +0530

    first commit

---
+++ WindowsFormsFileUpload/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace WindowsFormsFileUpload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(52, 34);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(301, 20);
+            this.txtBox1.TabIndex = 0;
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChoose.Location = new System.Drawing.Point(396, 34);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(130, 25);
+            this.btnChoose.TabIndex = 1;
+            this.btnChoose.Text = "Choose File";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(105, 99);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(546, 327);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnChoose);
+            this.Controls.Add(this.txtBox1);
+            this.Name = "Form1";
+            this.Text = "File Upload";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsFileUpload/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsFileUpload
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public bool ThumbnailCallback()
+        {
+            return false;
+        }
+
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Images(*.BMP; *.JPG; *.GIF,*.PNG,*.TIFF,*.JFIF) | *.BMP;*.JPG;*.GIF;*.PNG;*.TIFF,*.JFIF";
+            var result = fileDialog.ShowDialog();
+            if(result==DialogResult.OK)
+            {
+                var filepath = fileDialog.FileName;
+                txtBox1.Text = filepath;
+                
+                PictureBox imageControl = new PictureBox();
+                imageControl.Height = 500;
+                imageControl.Width = 500;
+
+                Image.GetThumbnailImageAbort myCallback = new Image.GetThumbnailImageAbort(ThumbnailCallback);
+                Bitmap myBitmap = new Bitmap(filepath);
+                Image myThumbnail = myBitmap.GetThumbnailImage(400, 300,myCallback, IntPtr.Zero);
+                imageControl.Image = myThumbnail;
+
+                pictureBox1.Controls.Add(imageControl);
+            }
+
+        }
+    }
+}
